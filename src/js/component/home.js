41c6c4ec753352e891b4f2a8aa60ec160865b130
tebanban <estebancalf@gmail.com>
2021-04-15 03:46:27 +0000

    basic functioning

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function App() {
+	const [currentItem, setCurrentItem] = useState(null);
+	const [itemList, setItemList] = useState([]);
+
+	const onChangeHandler = e => {
+		setCurrentItem(e.target.value);
+		//console.log("current", e.target.value);
+	};
+
+	const onSubmitHandler = e => {
+		setItemList([...itemList, currentItem]);
+		console.log(itemList);
+		setCurrentItem("");
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="wrapper">
+				<input
+					type="text"
+					value={currentItem}
+					onChange={onChangeHandler}
+				/>
+				<button onClick={onSubmitHandler}> + </button>
+				<ul>
+					<li>{itemList[0]}</li>
+					<li>{itemList[1]}</li>
+					<li>{itemList[2]}</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
